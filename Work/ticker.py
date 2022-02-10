f5e0c654557668c601ee9608158c03bef678641f
yomajo <ed.vinas@yahoo.com>
2022-02-10 12:07:16 +0200

    finished 6 module (generator expressions as last topic)

--- Work/ticker.py
+++ Work/ticker.py
@@ -12,34 +12,27 @@ def parse_stock_data(lines):
 
 def select_columns(rows, indices):
     for row in rows:
-        yield [row[index] for index in indices]
+        yield (row[index] for index in indices)
 
 def convert_types(rows, types):
     for row in rows:
-        yield [type_func(val) for type_func, val in zip(types, row)]
+        yield (type_func(val) for type_func, val in zip(types, row))
 
 def make_dicts(rows, headers):
     for row in rows:
         yield dict(zip(headers, row))
 
-def filter_symbols(rows, names):
-    for row in rows:
-        if row['name'] in names:
-            yield row
-
-def ticker(portfile:str='Data/portfolio.csv', logfile:str='Data/stocklog.csv', fmt:str='csv'):
+def ticker(portfile:str='Data/portfolio.csv', logfile:str='Data/stocklog.csv', fmt:str='txt'):
     portfolio = read_portfolio(portfile)
     lines = follow(logfile)
     formatter = create_formatter(fmt)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
+    rows = (row for row in rows if row['name'] in portfolio)
 
     formatter.headings(['Name', 'Price', 'Change'])
     for stock in rows:
         row_data = [stock['name'], str(stock['price']), str(stock['change'])]
         formatter.row(row_data)    
-
-
 
 if __name__ == '__main__':
     ticker()
