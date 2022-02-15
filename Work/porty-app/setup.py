7ac5d85a3bc67c103c6778d12911129d12d43af0
yomajo <ed.vinas@yahoo.com>
2022-02-15 13:09:55 +0200

    adding setup files. to package, use: "python setup.py sdist" to install: pip install [package_name.tar.gz]

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='porty',
+    version='0.0.1',
+    author='E.',
+    author_email='private_info@baby.com',
+    description='Practical Python code',
+    packages=setuptools.find_packages()
+)
